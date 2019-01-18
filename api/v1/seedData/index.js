f8c3fae3395ce23f4aebdfc1a54dbed7c6c428f5
lawal lanre <lanre.lawal@andela.com>
2019-01-18 20:56:51 +0100

    write test for contact controller

---
+++ api/v1/seedData/index.js
@@ -0,0 +1,35 @@
+export const contactSeeds = [
+  {
+    name: 'Test User 0',
+    phoneNumber: '08080000000',
+  },
+  {
+    name: 'Test User 1',
+    phoneNumber: '08080000001',
+  },
+  {
+    name: 'Test User 2',
+    phoneNumber: '08080000002',
+  },
+];
+
+export const messageSeeds = [
+  {
+    content: 'A simple message content 1',
+    senderId: '08080000000',
+    receiverId: '08080000001',
+    sentAt: Date(),
+  },
+  {
+    content: 'A simple message content 2',
+    senderId: '08080000001',
+    receiverId: '08080000002',
+    sentAt: Date(),
+  },
+  {
+    content: 'A simple message content 3',
+    senderId: '08080000001',
+    receiverId: '08080000000',
+    sentAt: Date(),
+  },
+];
